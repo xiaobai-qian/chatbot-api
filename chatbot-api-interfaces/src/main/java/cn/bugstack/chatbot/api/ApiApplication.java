0b8320ae84cc3899c10a51f61b4e9d055c03c214
Smile <1505023157@qq.com>
2024-02-29 16:17:43 +0800

    初始化项目工程

---
+++ chatbot-api-interfaces/src/main/java/cn/bugstack/chatbot/api/ApiApplication.java
@@ -0,0 +1,12 @@
+package cn.bugstack.chatbot.api;
+
+//启动入口
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
---
+++ chatbot-api-interfaces/src/test/java/cn/bugstack/chatbot/api/test/ApiTest.java
@@ -0,0 +1,6 @@
+package cn.bugstack.chatbot.api.test;
+
+//单元测试
+public class ApiTest {
+
+}
